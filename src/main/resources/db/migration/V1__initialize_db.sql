be67bc64a3dafe5a3cfb1809318edebce82d966e
Arina Ustyantseva <arina.ust@gmail.com>
2020-01-31 20:04:42 +0300

    added db migrations

---
+++ src/main/resources/db/migration/V1__initialize_db.sql
@@ -0,0 +1,3 @@
+DROP SCHEMA message_board IF EXISTS;
+
+CREATE SCHEMA message_board;
---
+++ src/main/resources/db/migration/V2__create_users_and_messages_tables.sql
@@ -0,0 +1,28 @@
+CREATE SEQUENCE message_board.s_users_id START WITH 1;
+
+CREATE TABLE message_board.users (
+    id INT NOT NULL DEFAULT NEXT VALUE FOR message_board.s_users_id,
+    username VARCHAR(50) NOT NULL,
+    passwordHash VARCHAR(4000) NOT NULL,
+
+    CONSTRAINT pk_t_users PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX i_t_users_username ON message_board.users (username);
+
+
+CREATE SEQUENCE message_board.s_messages_id START WITH 1;
+
+CREATE TABLE message_board.messages (
+    id INT NOT NULL DEFAULT NEXT VALUE FOR message_board.s_messages_id,
+    user_id INT NOT NULL,
+    title VARCHAR(4000) NOT NULL,
+    text CLOB NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    CONSTRAINT pk_t_messages PRIMARY KEY (id),
+    CONSTRAINT fk_t_messages_user_id FOREIGN KEY (user_id) REFERENCES message_board.users(id)
+);
+
+
