c412cbc2f64bf965cc6029cce238e94777bb7dc2
Arina Ustyantseva <arina.ust@gmail.com>
2020-02-03 18:09:05 +0300

    added authorization to security config

--- src/main/java/home/assignment/messageboard/configuration/SecurityConfiguration.java
+++ src/main/java/home/assignment/messageboard/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package home.assignment.messageboard.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/v1/**").authenticated()
+                .antMatchers("/v1/board/**").permitAll()
+                .antMatchers("/**").permitAll();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
