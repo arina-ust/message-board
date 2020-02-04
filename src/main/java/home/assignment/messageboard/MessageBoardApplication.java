54815ad503a6b5e8a6d3cf9f6a6d424954b6de50
Arina Ustyantseva <arina.ust@gmail.com>
2020-02-04 17:36:37 +0300

    fixed flyway on startup

--- src/main/java/home/assignment/messageboard/MessageBoardApplication.java
+++ src/main/java/home/assignment/messageboard/MessageBoardApplication.java
@@ -1,15 +1,25 @@
 package home.assignment.messageboard;
 
 import home.assignment.messageboard.configuration.ApplicationProperties;
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.util.ResourceBundle;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class MessageBoardApplication {
 
 	public static void main(String[] args) {
+		ResourceBundle applicationProperties = ResourceBundle.getBundle("application");
+		Flyway flyway = Flyway.configure().dataSource(applicationProperties.getString("spring.datasource.url"),
+				applicationProperties.getString("spring.datasource.username"),
+				applicationProperties.getString("spring.datasource.password"))
+				.load();
+		flyway.migrate();
+
 		SpringApplication.run(MessageBoardApplication.class, args);
 	}
 
