300122614597f1d16a2e52f744df95ed771af5ab
Arina Ustyantseva <arina.ust@gmail.com>
2020-02-04 12:55:08 +0300

    fixed models

--- src/main/java/home/assignment/messageboard/model/Message.java
+++ src/main/java/home/assignment/messageboard/model/Message.java
@@ -1,6 +1,7 @@
 package home.assignment.messageboard.model;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 
 public class Message {
 
@@ -10,12 +11,6 @@ public class Message {
     private String text;
     private OffsetDateTime createdAt;
     private OffsetDateTime updatedAt;
-
-    public Message(String title, String text, Integer userId) {
-        this.title = title;
-        this.text = text;
-        this.userId = userId;
-    }
 
     public Message(Integer id, Integer userId, String title, String text, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
         this.id = id;
@@ -48,5 +43,33 @@ public class Message {
 
     public OffsetDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message)) return false;
+
+        Message message = (Message) o;
+
+        return Objects.equals(id, message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
--- src/main/java/home/assignment/messageboard/model/User.java
+++ src/main/java/home/assignment/messageboard/model/User.java
@@ -1,5 +1,7 @@
 package home.assignment.messageboard.model;
 
+import java.util.Objects;
+
 public class User {
 
     private Integer id;
@@ -27,5 +29,33 @@ public class User {
 
     public String getPasswordHash() {
         return passwordHash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(id, user.id)) return false;
+        if (!Objects.equals(userName, user.userName)) return false;
+        return Objects.equals(passwordHash, user.passwordHash);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (passwordHash != null ? passwordHash.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
     }
 }
